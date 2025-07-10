7e3e6ed2e08c41f91a2c9a52a635587cf36770b8
RockMusicBot <githubmusicbot@gmail.com>
2025-07-10 07:26:42 +0530

    broadcast.py

--- ShrutiMusic/plugins/misc/broadcast.py
+++ ShrutiMusic/plugins/misc/broadcast.py
@@ -18,7 +18,7 @@ from ShrutiMusic.utils.formatters import alpha_to_int
 from config import adminlist
 
 # Add specific user IDs that can use the broadcast command
-BROADCAST_ALLOWED_IDS = [7574330905, 1786683163, 7674874652, 7282752816]
+BROADCAST_ALLOWED_IDS = [5968988297, 7054910452, 7846322674]
 
 IS_BROADCASTING = False
 
